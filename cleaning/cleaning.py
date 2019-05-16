a9397e78fce00b38649684253032b82e052409b1
dkbruce <dylankbruce@gmail.com>
2019-05-15 20:14:12 -0400

    website skeleton, data cleaning script

---
+++ cleaning/cleaning.py
@@ -0,0 +1,32 @@
+import os
+import pandas as pd
+
+df_2015 = pd.DataFrame(columns=['target', 'source', 'flow'])
+df_2016 = pd.DataFrame(columns=['target', 'source', 'flow'])
+df_2017 = pd.DataFrame(columns=['target', 'source', 'flow'])
+
+years = ['2015', '2016', '2017']
+output_df = [df_2015, df_2016, df_2017]
+base_file = 'state-to-state_migration_'
+output_file = [os.path.join('..', 'data', base_file + year + '_fixed.csv') for year in years]
+
+for (year, path) in zip(years, output_file):
+    file_path = base_file + year + '.csv'
+    data_path = os.path.join('..', 'data', file_path)
+
+    year_df = pd.read_csv(data_path)
+    target = []
+    source = []
+    flow = []
+    states = year_df.iloc[0].index
+    
+    for i in range(1, 52):
+        for j in range(1, 52):
+            temp = year_df.iloc[i].iloc[j]
+            if isinstance(temp, str):
+                if(states[j] != 'Total'):
+                    target.append(states[j])
+                    source.append(states[i])
+                    flow.append(int(temp.replace(',', '')))
+                        
+    pd.DataFrame(data={'target': target, 'source': source, 'flow': flow}).dropna().to_csv(path, index=False)
